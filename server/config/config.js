7736d593150badff87c9b81591631f37849b3047
Kenneth Siu <k1siu@ucsd.edu>
2017-05-23 11:37:24 -0700

    .

--- server/config/config.js
+++ server/config/config.js
@@ -5,4 +5,4 @@ module.exports = {
   mongoDBUrl: 'mongodb://cse112mao:teammao123@ds137891.mlab.com:37891/maoproj',
   port: process.env.PORT || 4941,
   secret: process.env.SECRET || 'mysecret'
-};
+}; // TEST
